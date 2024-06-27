9d4e7d58af02cc20e3b543a047525d18b112cf62
ayaarragab <ayarsk4343@gmail.com>
2024-06-28 01:53:50 +0300

    ES6 ... spread operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
+  for (const value of array) {
+    const index = array.indexOf(value);
+    array[index] = appendString + value; // eslint-disable-line no-param-reassign
   }
   return array;
 }
