b3a7cf0f93b8e8e88c71d0ada72a0b1b47b6df84
ayaarragab <ayarsk4343@gmail.com>
2024-06-28 19:12:58 +0300

    first 3 tasks

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,8 @@
 import ClassRoom from './0-classroom';
 
-const o1 = new ClassRoom(19);
-const o2 = new ClassRoom(20);
-const o3 = new ClassRoom(34);
-const initializeRooms = () => [o1, o2, o3]; // eslint-disable-line no-unused-vars
+export default function initializeRooms(){ // eslint-disable-line no-unused-vars
+    const o1 = new ClassRoom(19);
+    const o2 = new ClassRoom(20);
+    const o3 = new ClassRoom(34);
+    return [o1, o2, o3];
+}
