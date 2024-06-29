46a18ff977c80bb1e135d635921d8a16498dc86f
ayaarragab <ayarsk4343@gmail.com>
2024-06-29 10:23:04 +0300

    I cannot promise you

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error());
-    }
-  }).then(() => {
-    console.log('Got a response from the API');
-  });
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
