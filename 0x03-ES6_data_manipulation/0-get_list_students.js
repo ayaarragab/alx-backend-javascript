7336fbf15b66ae013f3680c8adcbe55c7d692779
ayaarragab <ayarsk4343@gmail.com>
2024-07-03 19:04:01 +0300

    conn

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,12 @@
 /**
  * Retrieves a list of students.
- *
+ * @author ayaarragab <https://github.com/ayaarragab>
  * @returns {Array} An array of student objects.
  */
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { firstName: "Guillaume", id: 1, location: 'San Francisco' },
+    { firstName: "James", id: 2, location: 'Columbia' },
+    { firstName: "Serana", id: 5, location: 'San Francisco' }
   ];
 }
