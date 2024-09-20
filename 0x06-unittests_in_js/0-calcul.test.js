3f18d20385d8cd14e04aac2615c5a23decd10278
ayaarragab <ayarsk4343@gmail.com>
2024-09-21 01:13:47 +0300

    edited things

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should return correct output', () => {
