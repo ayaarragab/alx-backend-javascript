8d55468bcd510c1343153aea0d646b395164e00e
ayaarragab <ayarsk4343@gmail.com>
2024-09-21 01:59:56 +0300

    found the bug

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,23 +1,25 @@
 const assert = require('assert');
+const { it, describe } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function () {
-  it('should return correct output', () => {
-    assert.equal(calculateNumber('SUM', 1.3, 5), 6);
-  });
-  it('should return correct output', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.6, 5), -3);
-  });
-  it('should return correct output', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.01, 0), 'Error');
-  });
-  it('should return correct output', () => {
-    assert.equal(calculateNumber('DIVIDE', 5.1, 2), 2.5);
-  })
-  it('0 and 0', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 0.0), 'Error');
-  });
-  it('checking correct type for operation 2', () => {
-    assert.equal(calculateNumber('plus', 1, 4), 'Error');
-  });
+
+describe('calculateNumber', () => {
+    it('checking if operation is correct 1', () => {
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+    it('checking if operation is correct 2', () => {
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('checking if operation is correct 3', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('checking if operation is correct 4', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+    it('checking correct type for operation 1', () => {
+        assert.equal(calculateNumber(5, 1, 4), 'Error');
+    });
+    it('checking correct type for operation 2', () => {
+        assert.equal(calculateNumber('plus', 1, 4), 'Error');
+    });
 });
