c42f4cfd72016839e3d415eb5f0dd44199fc95aa
ayaarragab <ayarsk4343@gmail.com>
2024-09-21 03:14:58 +0300

    spies

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,18 @@
-const sinon = require('sinon')
-const sendPaymentRequestToApi = require('./4-payment')
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment');
 const { expect } = require('chai');
 const { it, describe } = require('mocha');
 const Utils = require('./utils');
 
-describe('', () => {
+describe('sendPaymentRequestToApi', () => {
     const checkSoy = sinon.spy(console, 'log');
-    it('should', () => {
-        const stub = sinon.stub(Utils, 'calculateNumber').
-        sendPaymentRequestToApi(4, 5);
+    it('should log the correct total', () => {
+        const stub = sinon.stub(Utils, 'calculateNumber');
         stub.withArgs('SUM', 4, 5).returns(9);
+        sendPaymentRequestToApi(4, 5);
         expect(checkSoy.calledOnce).to.be.true;
-        expect(console.log('The total is: 9')).to.be.all;
+        expect(checkSoy.calledWith('The total is: 9')).to.be.true;
         checkSoy.restore();
         stub.restore();
-    })
-})
+    });
+});
