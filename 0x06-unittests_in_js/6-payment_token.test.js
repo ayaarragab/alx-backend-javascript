d049869a675686b034e217046b28a1590a4547e2
ayaarragab <ayarsk4343@gmail.com>
2024-09-21 03:28:36 +0300

    6th

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,24 @@
+const getPaymentTokenFromAPI = require('./6-payment');
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('isPromise?', () => {
+        res = getPaymentTokenFromAPI(true);
+        expect(res.to.be.instanceof(Promise));
+      });
+    it('', () => {
+        getPaymentTokenFromAPI(true)
+        .then((res) => {
+            expect(res.data).to.be.equal('Successful response from the API');
+            done();
+        });
+    });
+    it('', () => {
+        getPaymentTokenFromAPI(false)
+        .then((res) => {
+            expect(res.data).to.be.equal('');
+            done();
+        });
+    })
+});
